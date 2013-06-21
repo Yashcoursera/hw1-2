07fa01e042c3f40eb94957293c203f9693e04137
melwynt <melwynt@gmail.com>
2013-06-22 01:31:56 +0200

    Update hw1-2.js

--- hw1-2.js
+++ hw1-2.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-var arr = ["this is the first element", "this is the second element"];
+var arr = [];
+arr.push(Math.ceil(.95));
 console.log(arr[0]);              // prints "this is the first element"
-console.log(arr[1]);              // prints "this is the second element"
-console.log(arr[arr.length - 1]); // prints "this is the second element"
